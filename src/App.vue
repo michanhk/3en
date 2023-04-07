dfb26b80be181b7bde3d087fa966fac9241420f0
Lazy Itinerary <lazyitinerary@gmail.com>
2023-04-08 03:00:07 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
 
 <template>
   <div class="my-0 py-0">
-    <v-row class="my-0 py-0 pl-3" id="headerr">
+    <v-row class="my-0 py-0 pl-3" id="div-header">
       <v-col class="my-0 py-0" style="line-height: 0"
-        ><img alt="Vue logo" src="@/assets/logo.png" width="65" height="65"
+        ><img src="@/assets/logo.png" width="65" height="65"
       /></v-col>
       <v-col class="my-0 py-0 text-button"
         ><v-container class="text-right"
@@ -16,18 +16,164 @@
     </v-row>
     <v-row style="height: 90vh" class="my-0 py-0">
       <v-img
-        cover
-        alt="Vue logo"
+        cover        
         src="https://www.odetterestaurant.com/wp-content/uploads/2017/03/odette-home-desktop-bg-1.jpg"
       />
     </v-row>
+    <v-row style="height: 5vh" class="my-0 py-0">      
+    </v-row>
+    <div class="my-0 py-0 pl-3">
+      <p class="pb-1 text-center" style="font-size: 16px; color: #8E8096; letter-spacing: 1.5px;">OUR PEOPLE</p>
+      <p class="text-center pb-2" style="font-size: 10px; color: #A6AEB0">OUR PEOPLE<span class="px-2"></span>•<span class="px-2"></span>PEOPLE WE ADMIRE<span class="px-2"></span>•<span class="px-2"></span>WORK WITH US<span class="px-2"></span></p>
+      <v-row style="height: 30vh" class="my-0 py-0">
+      <v-img
+        cover        
+        src="https://www.odetterestaurant.com/wp-content/uploads/2017/03/Odette_Adam-Wan-MOBILE-1-1024x780.jpg"
+      />
+    </v-row>      
+    <div>
+      <p class="pt-3 pb-1 text-left" style="font-size: 16px; color: #8E8096; letter-spacing: 1.5px;">ADAM WAN, HEAD CHEF</p>
+    </div>
+    </div>
+    <v-row style="height: 5vh" class="my-0 py-0">      
+    </v-row>
+    <v-row style="height: 30vh" class="my-0 py-0">
+      <v-img
+        cover        
+        src="https://www.odetterestaurant.com/wp-content/uploads/2017/03/odette-abtinspiration-mobile-bg-1-768x422.jpg"
+      />
+    </v-row>    
+    <div class="my-0 py-0 pl-3" id="div-row1">
+      <p class="text-center py-2" style="font-size: 10px">INSPIRATION<span class="px-2"></span>•<span class="px-2"></span>CUISINE<span class="px-2"></span>•<span class="px-2"></span>ART<span class="px-2"></span>•<span class="px-2"></span>INTERIOR DESIGN</p>
+    </div>    
+    <div class="my-0 py-0 pt-6 pl-3">
+      <span class="text-left py-3 pb-1" style="font-size: 16px; color: #8E8096; letter-spacing: 1.5px; border-bottom: 1px solid #6d6e71">INSPIRATION</span>
+      <p class="text-left pt-6 pb-1" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">
+        “I owe everything that I am to my family, especially my grandmother, Odette. She showed me how the most remarkable dishes can come from the purest ingredients and taught me the importance of adding that ‘little something’ to create dishes that excite the palate and fill the heart.
+      </p>
+      <p class="text-left py-3" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">        
+        Her devotion to people and ability to demonstrate love through food continue to inspire the Odette experience.”
+      </p>
+      <p class="text-left py-3 font-italic font-weight-thin" style="font-size: 14px; color: #8E8096;">Julien Royer</p>
+    </div>      
+    <v-row style="height: 5vh" class="my-0 py-0">      
+    </v-row>    
+    <v-row style="height: 30vh" class="my-0 py-0">
+      <v-img
+        cover        
+        src="https://www.odetterestaurant.com/wp-content/uploads/2017/03/odette-abtart-mobile-bg-1-768x422.jpg"
+      />
+    </v-row>    
+    <div class="my-0 py-0 pl-3" id="div-row1">
+      <p class="text-center py-2" style="font-size: 10px">INSPIRATION<span class="px-2"></span>•<span class="px-2"></span>CUISINE<span class="px-2"></span>•<span class="px-2"></span>ART<span class="px-2"></span>•<span class="px-2"></span>INTERIOR DESIGN</p>
+    </div>    
+    <div class="my-0 py-0 pt-6 pl-3">
+      <span class="text-left py-3 pb-1" style="font-size: 16px; color: #8E8096; letter-spacing: 1.5px; border-bottom: 1px solid #6d6e71">ART</span>
+      <p class="text-left pt-6 pb-1" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">
+        Led by Singaporean artist, Dawn Ng, the overall creative direction stems from a deep understanding of Chef Julien’s culinary philosophy.
+      </p>
+      <p class="text-left py-3" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">        
+        Titled A THEORY OF EVERYTHING, the body of art explores the visual beauty of raw ingredients from Chef Julien’s kitchen, broken down and re-imagined into a surreal universe of shapes and forms that float and settle, drift and pirouette.
+      </p>
+      <p class="text-left py-3" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">        
+        These deconstructed, magnified images of Chef Julien’s most precious ingredients inform every aspect of Odette’s creative design, including the aerial installation – the centerpiece of the main dining hall.
+      </p>      
+      <p class="text-left py-3 font-italic font-weight-thin" style="font-size: 14px; color: #8E8096;">Julien Royer</p>
+    </div>      
+    <v-row style="height: 5vh" class="my-0 py-0">      
+    </v-row>        
+    <v-row style="height: 30vh" class="my-0 py-0">
+      <v-img
+        cover        
+        src="https://www.odetterestaurant.com/wp-content/uploads/2017/03/odette-reservations-mobile-bg-1-768x422.jpg"
+      />
+    </v-row>      
+    <div class="my-0 py-0 pt-6 pl-3">
+      <span class="text-left py-3 pb-1" style="font-size: 16px; color: #8E8096; letter-spacing: 1.5px; border-bottom: 1px solid #6d6e71">RESERVATIONS</span>
+    </div>      
+    <div class="my-0 py-0 pt-6 pl-3">
+      <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">LUNCH</p>
+      <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">Terre & Mer: 328 (5 Acts)</p>
+      <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">Epicure: 488 (7 Acts)</p>
+      <p class="py-2"></p>
+      <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">DINNER</p>
+      <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">Nature et Découverte : 348 (7 Acts)</p>
+      <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">Epicure: 488 (7 Acts)</p>
+      <p class="py-2"></p>
+      <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">Please note:</p>      
+      <p class="py-1"></p>
+      <v-row class="ma-0 pa-0">
+        <v-col class="ma-0 pa-0" cols="1">
+          <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px"><v-icon size="x-small" icon="mdi-chevron-down" /></p>      
+        </v-col>
+        <v-col class="ma-0 pa-0" cols="11">
+          <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">Reservations are accepted up to 60 days in advance. We advise making a reservation 1 month in advance to secure a table.</p>      
+        </v-col>
+      </v-row>
+      <v-row class="ma-0 pa-0">
+        <v-col class="ma-0 pa-0" cols="1">
+          <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px"><v-icon size="x-small" icon="mdi-chevron-down" /></p>      
+        </v-col>
+        <v-col class="ma-0 pa-0" cols="11">
+          <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">We offer only tasting menus for lunch and dinner, there are no other menu formats available.</p>      
+        </v-col>
+      </v-row>
+      <v-row class="ma-0 pa-0">
+        <v-col class="ma-0 pa-0" cols="1">
+          <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px"><v-icon size="x-small" icon="mdi-chevron-down" /></p>      
+        </v-col>
+        <v-col class="ma-0 pa-0" cols="11">
+          <p class="text-left" style="font-size: 12px; color: #8E8096; letter-spacing: 0.25px">Our dress code is smart casual though we strongly recommend dressing up for the occasion. Gentlemen, we ask that you come dressed in trousers and appropriate footwear.</p>      
+        </v-col>
+      </v-row>
+      <p class="py-2"></p>
+      <div class="pt-3">
+        <v-btn rounded="0" elevation="0" size="small" variant="outlined" style="color: #8E8096; border-color: #8E8096" href="https://www.rosewoodhotels.com/en/hong-kong/weddings" target="_blank">MAKE YOUR RESERVATION</v-btn>
+      </div>
+    </div>  
+    <v-row style="height: 5vh" class="my-0 py-0">      
+    </v-row>         
+    <v-row style="height: 30vh" class="my-0 py-0">
+      <v-img
+        cover        
+        src="https://www.odetterestaurant.com/wp-content/uploads/2017/03/odette-contactus-mobile-bg-768x422.jpg"
+      />
+    </v-row>      
+    <div class="my-0 py-0 pt-6 pl-3">
+      <span class="text-left py-3 pb-1" style="font-size: 16px; color: #8E8096; letter-spacing: 1.5px; border-bottom: 1px solid #6d6e71">GETTING HERE</span>
+      <p class="py-2"></p>   
+      <v-row style="height: 50vh" class="my-0 py-0">
+        <v-img
+          cover        
+          src="https://www.odetterestaurant.com/wp-content/uploads/2017/03/Odette-Map-1024x808.png"
+        />
+      </v-row>            
+      <div class="pt-3">
+        <v-btn rounded="0" elevation="0" size="small" variant="outlined" style="color: #8E8096; border-color: #8E8096" href="https://www.google.com/maps/place/Rosewood+Hong+Kong/@22.2949813,114.1736218,17z/data=!3m1!4b1!4m9!3m8!1s0x340400f1bafdc8e1:0x2d065a769eb1949f!5m2!4m1!1i2!8m2!3d22.2949764!4d114.1758105!16s%2Fg%2F11fd47y413?hl=en" target="_blank">OPEN GOOGLE MAP</v-btn>
+      </div>      
+    </div>       
+    <v-row style="height: 5vh" class="my-0 py-0">      
+    </v-row>     
+    <v-row class="my-0 py-6 pl-3" id="div-header" style="height: 12vh" >
+      <v-img src="https://www.odetterestaurant.com/wp-content/uploads/2017/03/ODT%E2%80%94Mobile_Footer_2022_White-768x181.png"></v-img>
+    </v-row>           
   </div>
 </template>
 
 <style scoped>
-#headerr {
+#div-header {
   background-color: #dec7b6;
   color: #ffffff;
+}
+
+#div-row1 {
+  background-color: #F4F4F4;
+  color: #A6AEB0;
+  text-align: center;  
+}
+
+.p{
+  font-family: GeoSansLight;
 }
 
 .logo {
